899c04715c57f51ce6529e222ebac4e5f95e732e
maowangxin <maowangxin@globalegrow.com>
2021-05-20 18:38:32 +0800

    init_flutter_demo

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+/*
+main() {
+  runApp(
+      MaterialApp(
+          debugShowMaterialGrid: false,
+          home: Scaffold(
+            appBar: AppBar(
+              title: Text("ZAFUL跨境电商品牌"),
+            ),
+            body: Center(
+              child: Text(
+                "ZAFUL iOS App",
+                style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.black
+                ),
+              ),
+            ),
+          )
+      )
+  );
+}*/
+
+main () => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
