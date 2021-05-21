abac201e86f8cac27eb11ce7604b0b34ba47f477
maowangxin <maowangxin@globalegrow.com>
2021-05-22 00:49:44 +0800

    双按钮计数器示例

--- lib/HelloFlutter/05_双按钮计数器.dart
+++ lib/HelloFlutter/05_双按钮计数器.dart
@@ -20,8 +20,8 @@ class WXHomePage extends StatelessWidget {
         title: Text(
           "双按钮计数器",
           style: TextStyle(
-            fontSize: 20,
-            color: Colors.amberAccent
+              fontSize: 20,
+              color: Colors.amberAccent
           ),
         ),
       ),
@@ -29,37 +29,37 @@ class WXHomePage extends StatelessWidget {
     );
   }
 }
- class WXHomePageBody extends StatefulWidget {
-   @override
-   _WXHomePageBodyState createState() => _WXHomePageBodyState();
- }
- 
- class _WXHomePageBodyState extends State<WXHomePageBody> {
+class WXHomePageBody extends StatefulWidget {
+  @override
+  _WXHomePageBodyState createState() => _WXHomePageBodyState();
+}
+
+class _WXHomePageBodyState extends State<WXHomePageBody> {
   var count = 0;
 
-   @override
-   Widget build(BuildContext context) {
-     return Center(
-       child: Column(
-         mainAxisAlignment: MainAxisAlignment.center,
-         children: [
-           Row(
-             mainAxisAlignment: MainAxisAlignment.center,
-             children: [
-               _getRaseButton(true),
-               _getRaseButton(false),
-             ],
-           ),
-           Text("计数器操作: $count", style: TextStyle(
-             fontSize: 25,
-             color: Colors.blue
-           ),)
-         ],
-       ),
-     );
-   }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _getRaseButton(true),
+              _getRaseButton(false),
+            ],
+          ),
+          Text("计数器操作: $count", style: TextStyle(
+              fontSize: 25,
+              color: Colors.blue
+          ),)
+        ],
+      ),
+    );
+  }
 
-   RawMaterialButton _getRaseButton(bool isAdd) {
+  RawMaterialButton _getRaseButton(bool isAdd) {
     return RawMaterialButton(
         fillColor: isAdd ? Colors.red : Colors.blue,
         child: Text(
@@ -80,5 +80,5 @@ class WXHomePage extends StatelessWidget {
         });
   }
 }
- 
+
 
