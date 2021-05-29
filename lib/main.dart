d8236d11792114d715bb8b88c178017b43e30eff
maowangxin <maowangxin@globalegrow.com>
2021-05-29 16:17:45 +0800

    addfile

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(MyApp());
@@ -12,44 +11,11 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class WXHomePage extends StatefulWidget {
-  @override
-  _WXHomePageState createState() => _WXHomePageState();
-}
-
-class _WXHomePageState extends State<WXHomePage> {
+class WXHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-<<<<<<< HEAD
-        title: Text(
-          "各种控件演练",
-          style: TextStyle(fontSize: 30, color: Colors.white),
-        ),
-      ),
-      body: Column(
-        children: [
-          _testBuildText(),
-          Image(image: AssetImage("assets/images/computer.png"))
-        ],
-      ),
-    );
-  }
-
-  Text _testBuildText() {
-    return Text(
-          "《定风波》 苏轼 莫听穿林打叶声，何妨吟啸且徐行。竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。",
-          textAlign: TextAlign.center,
-          maxLines: 3,
-          overflow: TextOverflow.ellipsis,//一行显示不下就显示三个点
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.red,
-            fontWeight: FontWeight.bold
-          ),
-        );
-=======
         title: Text("基础Widget"),
       ),
       body: WXHomeContent(),
@@ -71,6 +37,5 @@ class _WXHomeContentState extends State<WXHomeContent> {
   @override
   Widget build(BuildContext context) {
     return Container();
->>>>>>> renamefileanme
   }
 }
---
+++ lib/studyFlutter/06_TextWidget示例.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WXHomePage(),
+    );
+  }
+}
+
+class WXHomePage extends StatefulWidget {
+  @override
+  _WXHomePageState createState() => _WXHomePageState();
+}
+
+class _WXHomePageState extends State<WXHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "各种控件演练",
+          style: TextStyle(fontSize: 30, color: Colors.white),
+        ),
+      ),
+      body: Column(
+        children: [
+          _testBuildText(),
+          Image(image: AssetImage("assets/images/computer.png"))
+        ],
+      ),
+    );
+  }
+
+  Text _testBuildText() {
+    return Text(
+      "《定风波》 苏轼 莫听穿林打叶声，何妨吟啸且徐行。竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。",
+      textAlign: TextAlign.center,
+      maxLines: 3,
+      overflow: TextOverflow.ellipsis, //一行显示不下就显示三个点
+      style: TextStyle(
+          fontSize: 20,
+          color: Colors.red,
+          fontWeight: FontWeight.bold
+      ),
+    );
+  }
+}
